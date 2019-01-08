999151c33474e50f7fb54ee0d5561575925eaaec
MengluShi <2574452601@qq.com>
2019-01-08 14:33:09 +0800

    引入jQuery

--- src/components/Data/Pie3Dchart.vue
+++ src/components/Data/Pie3Dchart.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="chartHeight" :id="id" :option="option"></div>
+    <div class="chartHeight" :id="id"></div>
   </div>
 </template>
 <script>
@@ -10,16 +10,57 @@ export default {
     id: {
       type: String
     },
-    //option 是图表的配置数据
-    option: {
-      type: Object
-    }
+  },
+  data() {
+    return {
+      option: {
+        colors: [
+          "#d32279",
+          "#007ddf",
+        ],
+        chart: {
+          backgroundColor: null,
+          type: "pie", //饼图
+          options3d: {
+            enabled: true, //使用3d功能
+            alpha: 60, //延y轴向内的倾斜角度
+            beta: 0
+          }
+        },
+        title: {
+          text: "", //图表的标题文字
+          style: {
+            color: "#fff"
+          },
+          align: "left"
+        },
+        plotOptions: {
+          pie: {
+            allowPointSelect: true, //每个扇块能否选中
+            cursor: "pointer", //鼠标指针
+            depth: 20, //饼图的厚度
+            dataLabels: {
+              enabled: true //是否显示饼图的线形tip
+            }
+          }
+        },
+        series: [
+          {
+            type: "pie",
+            data: [
+              ["女", 12], //模块名和所占比，也可以{name: '测试1',y: 12}
+              ["男", 23],
+            ]
+          }
+        ]
+      }
+    };
   },
   mounted() {
     HighCharts.chart(this.id, this.option);
-     const vm = this;
-      // 基于准备好的dom，初始化echarts实例
-      var myChart = HighCharts.chart(this.id, this.option);
+    const vm = this;
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = HighCharts.chart(this.id, this.option);
     vm.$nextTick(() => {
       vm.drawChart();
     });
--- src/pages/Data/Data.vue
+++ src/pages/Data/Data.vue
@@ -5,88 +5,46 @@
         <img src="../../assets/images/title.png" alt>
       </div>
       <span class="back-index">返回首页</span>
-      <el-col class="common-height" :span="5">
-        <div class="bili-chart">
-          <Pie3DChart class="pie3DChart" :id="id" :option="option"/>
-        </div>
-        <div class="data-table">1</div>
-        <div class="common-chart">1</div>
-      </el-col>
-      <el-col class="common-height" :span="14">
-        <div class="data-info">1</div>
-        <div class="map-chart">1</div>
-        <div class="common-chart">1</div>
-      </el-col>
-      <el-col class="common-height" :span="5">
-        <div class="common-chart">1</div>
-        <div class="common-chart">1</div>
-        <div class="common-chart">1</div>
-      </el-col>
+      <div class="main-view common-height">
+        <el-col class="common-height" :span="6">
+          <div class="bili-chart">
+            <Pie3DChart class="pie3DChart" :id="id"/>
+          </div>
+          <div class="data-table">
+            <TableCompent/>
+          </div>
+          <div class="common-chart">
+              <BarChart :chartData ="chartData1"/>
+          </div>
+        </el-col>
+        <el-col class="common-height" :span="12">
+          <div class="data-info">1</div>
+          <div class="map-chart">1</div>
+          <div class="common-chart">1</div>
+        </el-col>
+        <el-col class="common-height" :span="6">
+          <div class="common-chart">1</div>
+          <div class="common-chart">1</div>
+          <div class="common-chart">1</div>
+        </el-col>
+      </div>
     </div>
   </div>
 </template>
 <script>
-import Pie3DChart from "../../components/Data/Pie3Dchart"
+import Pie3DChart from "../../components/Data/Pie3Dchart";
+import TableCompent from "../../components/Data/TableComponent";
+import BarChart from "../../components/Data/BarChart"
 export default {
   name: "Data",
   data() {
     return {
       screenHeight: document.documentElement.clientHeight, //屏幕高度
-      option: {
-        colors: [
-          "#c73e6a",
-          "#e192a8",
-          "#ff8b4e",
-          "#fbd884",
-          "#08b170",
-          "#00b6e8",
-          "#8d7ce4"
-        ],
-        chart: {
-          backgroundColor: null,
-          type: "pie", //饼图
-          options3d: {
-            enabled: true, //使用3d功能
-            alpha: 60, //延y轴向内的倾斜角度
-            beta: 0
-          }
-        },
-        title: {
-          text: "重点事件比例分布", //图表的标题文字
-          style: {
-            color: "#fff"
-          },
-          align: "left"
-        },
-        plotOptions: {
-          pie: {
-            allowPointSelect: true, //每个扇块能否选中
-            cursor: "pointer", //鼠标指针
-            depth: 15, //饼图的厚度
-            dataLabels: {
-              enabled: true //是否显示饼图的线形tip
-            }
-          }
-        },
-        series: [
-          {
-            type: "pie",
-            // name: "测试用1", //统一的前置词,非必须
-            data: [
-              ["待接收", 12], //模块名和所占比，也可以{name: '测试1',y: 12}
-              ["待派遣", 23],
-              ["待处理", 19],
-              ["已废弃", 29],
-              ["待核查", 29],
-              ["待结案", 29],
-              ["待延期审核", 29]
-            ]
-          }
-        ]
-      }
+      id: "biliChart",
+      chartData1 :[100,123,112,124,132,121,114]
     };
   },
-  components: {Pie3DChart},
+  components: { Pie3DChart, TableCompent,BarChart },
   mounted() {
     var _this = this;
     //页面加载时赋值id全屏高度
@@ -96,7 +54,7 @@ export default {
       // 定义窗口大小变更通知事件
       _this.screenHeight = document.documentElement.clientHeight; //窗口高度
     };
-    //echart操作
+    // //echart操作
     const vm = this;
     vm.$nextTick(() => {});
   },
@@ -118,6 +76,7 @@ export default {
   background: url("../../assets/images/data_bg.png") no-repeat;
   background-size: 100% 100%;
   color: #fff;
+  overflow: hidden;
 }
 .header {
   padding: 30px 0;
@@ -141,14 +100,24 @@ export default {
   background: url("../../assets/images/back_index_2.png") no-repeat;
   background-size: 100% 100%;
 }
+.main-view {
+  width: 100%;
+  height: 100%;
+  padding: 30px;
+}
 .common-height {
   height: calc(100% - 108px);
 }
 .bili-chart {
   height: 28%;
+  background: url("../../assets/images/bili_bg.png") no-repeat;
+  background-size: 100% 100%;
+  width: 90%;
+  margin: 0 auto;
 }
 .bili-chart div {
-  height: 100%;
+  height: 90%;
+  padding-top: 6%;
 }
 .data-table {
   height: 40%;
@@ -156,10 +125,19 @@ export default {
 .common-chart {
   height: 32%;
 }
+.common-chart div{
+    height: 100%;
+}
 .map-chart {
   height: 46%;
 }
 .data-info {
   height: 20%;
+}
+.data-table {
+  background: url("../../assets/images/daiban_bg.png") no-repeat;
+  background-size: 100% 100%;
+  width: 90%;
+  margin: 1% auto;
 }
 </style>
