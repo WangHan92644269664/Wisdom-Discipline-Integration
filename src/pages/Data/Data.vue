726a3c20e71a1a5483c93621c2e9a22a4e596f2d
MengluShi <2574452601@qq.com>
2019-01-08 10:05:34 +0800

    增加了大数据页面

---
+++ src/pages/Data/Data.vue
@@ -0,0 +1,11 @@
+<template>
+   <div>我是大数据页面</div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Header from '@/components/Header'
+import Data from "../pages/Data/Data"
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Header',
       component:Header
+    },
+    {
+      path: '/data',
+      name: 'Header',
+      component:Data
     }
   ]
 })
