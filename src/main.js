14b50c091622fc044330b8f83cdcca6ea58ec9ed
MengluShi <2574452601@qq.com>
2019-01-08 13:10:05 +0800

    引入highchart

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+// 引入highchart
+import highcharts3d from 'highcharts/highcharts-3d'
+highcharts3d(highcharts)
 //引入echarts 
 import echarts from 'echarts'
 Vue.prototype.$echarts = echarts
--- src/pages/Data/Data.vue
+++ src/pages/Data/Data.vue
@@ -1,22 +1,37 @@
 <template>
-   <div>
-       <div class="content" id="content" ref="homePage">
-111
-       </div>
-   </div>
+  <div>
+    <div class="content" id="content" ref="homePage">
+      <div class="header">
+        <img src="../../assets/images/title.png" alt>
+      </div>
+      <span class="back-index">返回首页</span>
+      <el-col class="common-height" :span="5">
+        <div class="bili-chart">1</div>
+        <div class="data-table">1</div>
+        <div class="common-chart">1</div>
+      </el-col>
+      <el-col class="common-height" :span="14">
+        <div class="data-info">1</div>
+        <div class="map-chart">1</div>
+        <div class="common-chart">1</div>
+      </el-col>
+      <el-col class="common-height" :span="5">
+        <div class="common-chart">1</div>
+        <div class="common-chart">1</div>
+        <div class="common-chart">1</div>
+      </el-col>
+    </div>
+  </div>
 </template>
 <script>
 export default {
   name: "Data",
   data() {
     return {
-      screenHeight: document.documentElement.clientHeight, //屏幕高度
-     
+      screenHeight: document.documentElement.clientHeight //屏幕高度
     };
   },
-  components: {
-   
-  },
+  components: {},
   mounted() {
     var _this = this;
     //页面加载时赋值id全屏高度
@@ -37,16 +52,56 @@ export default {
       oIframe.style.height = Number(val) + "px"; //'120'是页面布局调整，可去除
     }
   },
-  created() {
-
-  },
+  created() {},
   methods: {}
 };
 </script>
 <style scoped>
-.content{
-    width:100%;
-    height: 100%;
-    background:url('../../assets/images/data_bg.png') no-repeat;
+.content {
+  width: 100%;
+  height: 100%;
+  background: url("../../assets/images/data_bg.png") no-repeat;
+  background-size: 100% 100%;
+  color: #fff;
+}
+.header {
+  padding: 30px 0;
+}
+.header img {
+  width: auto;
+}
+.back-index {
+  color: #fff;
+  position: absolute;
+  top: 50px;
+  left: 200px;
+  display: inline-block;
+  width: 100px;
+  height: 40px;
+  background: url("../../assets/images/backIndex_1.png") no-repeat;
+  background-size: 100% 100%;
+  line-height: 40px;
+}
+.back-index:hover {
+  background: url("../../assets/images/back_index_2.png") no-repeat;
+  background-size: 100% 100%;
+}
+.common-height {
+  height: calc(100% - 108px);
+}
+.bili-chart {
+  height: 28%;
+}
+.data-table {
+  height: 40%;
+}
+.common-chart {
+  height: 32%;
+}
+.map-chart {
+  height: 46%;
+}
+.data-info {
+  height: 20%;
 }
 </style>
