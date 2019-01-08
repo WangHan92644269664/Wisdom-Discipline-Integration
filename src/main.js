2684391e1afbf77bb3a57c6365b5157903603af8
MengluShi <2574452601@qq.com>
2019-01-08 10:18:40 +0800

    引入echarts

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-
+//引入echarts 
+import echarts from 'echarts'
+Vue.prototype.$echarts = echarts
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 /* eslint-disable no-new */
